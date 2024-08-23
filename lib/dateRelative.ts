678294a4464790b0a7fcce628de462205dc19466
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-08-23 14:49:45 -0300

    change localization to pt-BR

--- lib/dateRelative.ts
+++ lib/dateRelative.ts
@@ -1,7 +1,9 @@
 import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
+import localePtBr from 'date-fns/locale/pt-BR';
 
 export default function distanceToNow(dateTime: number | Date) {
   return formatDistanceToNowStrict(dateTime, {
     addSuffix: true,
+    locale: localePtBr
   });
 }
